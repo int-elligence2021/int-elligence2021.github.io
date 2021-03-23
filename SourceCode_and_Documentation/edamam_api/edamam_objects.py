c91e47b0aa03fbb0aa307cc890a8b5051cb68d57
Joel Huang <z5309478@ad.unsw.edu.au>
2021-03-23 22:40:47 +1100

    Added API keys & id

--- SourceCode_and_Documentation/edamam_api/edamam_objects.py
+++ SourceCode_and_Documentation/edamam_api/edamam_objects.py
@@ -1,11 +1,12 @@
 from py_edamam import PyEdamam
 
-e = PyEdamam(nutrition_appid='xxx',
-           nutrition_appkey='xxx',
-           recipes_appid='xxx',
-           recipes_appkey='xxx',
-           food_appid='xxx',
-           food_appkey='xxx')
+e = PyEdamam(
+            nutrition_appid='1b7015b5',
+            nutrition_appkey='cd0b1be36422d9ec8114e424b2a76aa9',
+            recipes_appid='ed9ebd49',
+            recipes_appkey='faa479368d9dd0d427347cfb1a32f2aa	',
+            food_appid='a8a3b6cb',
+            food_appkey='80f9ae15038451b2095a0fe982980f37')
 
 for recipe in e.search_recipe("onion and chicken"):
     print(recipe)
