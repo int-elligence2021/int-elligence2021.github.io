a36bebc28f118131ef415b6a7d1a7c0241c7c890
Rohan Warrier <z5312990@ad.unsw.edu.au>
2021-03-23 14:31:07 +1100

    remove item from list option added

--- SourceCode_and_Documentation/search.js
+++ SourceCode_and_Documentation/search.js
@@ -5,6 +5,7 @@
 */
 
 
+/* Add ingredients after typing them to the list" */
 var button_add = document.getElementById("enteringredients");
 var input_add = document.getElementById("typeingredient");
 var ul_add = document.querySelector("#add_ingredients_list");
@@ -13,10 +14,12 @@ button_add.addEventListener("click", function() {
   var li_add = document.createElement("li");
   li_add.className = 'list-group-item';
   li_add.textContent = input_add.value;
+  li_add.setAttribute('onclick','remove_list_item(this)');
   ul_add.appendChild(li_add);
 })
 
 
+/* Add ingredients to negative search list after typing them */
 var button_rem = document.getElementById("remingredient");
 var input_rem = document.getElementById("typerem");
 var ul_rem = document.querySelector("#rem_ingredients_list");
@@ -25,5 +28,12 @@ button_rem.addEventListener("click", function() {
   var li_rem = document.createElement("li");
   li_rem.className = 'list-group-item';
   li_rem.textContent = input_rem.value;
+  li_rem.setAttribute('onclick','remove_list_item(this)');
   ul_rem.appendChild(li_rem);
 })
+
+
+/* removes current item from the parent node */
+function remove_list_item(item) {
+  item.parentNode.removeChild(item)
+}
