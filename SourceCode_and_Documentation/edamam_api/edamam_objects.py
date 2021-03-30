7d9e70ee1b31b4924c546dd5233478e826d14c95
int-elligence2021 <76695700+int-elligence2021@users.noreply.github.com>
2021-03-30 14:12:44 +1100

    Update edamam_objects.py

--- SourceCode_and_Documentation/edamam_api/edamam_objects.py
+++ SourceCode_and_Documentation/edamam_api/edamam_objects.py
@@ -4,7 +4,7 @@ e = PyEdamam(
             nutrition_appid='1b7015b5',
             nutrition_appkey='cd0b1be36422d9ec8114e424b2a76aa9',
             recipes_appid='ed9ebd49',
-            recipes_appkey='faa479368d9dd0d427347cfb1a32f2aa	',
+            recipes_appkey='faa479368d9dd0d427347cfb1a32f2aa',
             food_appid='a8a3b6cb',
             food_appkey='80f9ae15038451b2095a0fe982980f37')
 
