966336574bdb856a82df563fe2a09ec550b05cca
Rohan Warrier <z5312990@ad.unsw.edu.au>
2021-03-31 00:25:10 +1100

    added post method and way of extracting form data

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,8 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/results')
+@app.route('/results', methods=["POST", "GET"])
 def results_page():
+    if request.method == "POST":
+        print(f"Dietary Requirements: {request.form.getlist('dietary_req')}")
+        print(f"Nutritional Requirements: {request.form.getlist('nut_req')}")
+        print(f"Cuisine: {request.form.get('cuisine')}")
+        print(f"Dish type: {request.form.get('mealtype')}")
     return render_template('results_page.html')
 
 @app.route('/display')
