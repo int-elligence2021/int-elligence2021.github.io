5a593251181921be385c9c73ef59fae2cd4fb8dc
Rohan Warrier <z5312990@ad.unsw.edu.au>
2021-03-30 23:28:04 +1100

    website running through flask server

--- server.py
+++ server.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/results')
+def results_page():
+    return render_template('results_page.html')
+
+@app.route('/display')
+def display_page():
+    return render_template('display_page.html')
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True) #TODO: remove debug=True before deployment
